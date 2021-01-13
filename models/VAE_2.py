3082e4735c34b73cd6b0a71a137a75eb9644a0bb
Frano Rajič <frano.rajich@gmail.com>
2021-01-13 18:35:30 +0100

    Minor refactor started

--- models/VAE_2.py
+++ models/VAE_2.py
@@ -1,38 +1,8 @@
-import glob
-
-import PIL
-import imageio
-import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
-import tensorflow_probability as tfp
-import time
 
 from utils.GatedDenseLayer import GatedDenseLayer
 from utils.Hardtanh import Hardtanh
-
-tf.random.set_seed(72)
-np.random.seed(72)
-
-(train_images, _), (test_images, _) = tf.keras.datasets.mnist.load_data()
-
-
-def preprocess_images(images):
-    images = images.reshape((images.shape[0], 28, 28, 1)) / 255.
-    return np.where(images > .5, 1., 0.).astype("float32")
-
-
-print("Before preprocessing:", train_images.shape)
-train_images = preprocess_images(train_images)
-test_images = preprocess_images(test_images)
-print("After preprocessing:", train_images.shape)
-
-train_size = 60000
-batch_size = 32
-test_size = 10000
-
-train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(train_size).batch(batch_size)
-test_dataset = tf.data.Dataset.from_tensor_slices(test_images).shuffle(test_size).batch(batch_size)
 
 
 class CVAE(tf.keras.Model):
@@ -143,9 +113,6 @@ class VAE(tf.keras.Model):
         return logits
 
 
-optimizer = tf.keras.optimizers.Adam(1e-4)
-
-
 def log_normal_pdf(sample, mean, logvar, raxis=1):
     log2pi = tf.math.log(2. * np.pi)
     return tf.reduce_sum(-.5 * ((sample - mean) ** 2. * tf.exp(-logvar) + logvar + log2pi), axis=raxis)
@@ -160,132 +127,3 @@ def compute_loss(model, x):
     logz = log_normal_pdf(z, 0., 0.)
     logqz_x = log_normal_pdf(z, mean, logvar)
     return -tf.reduce_mean(logpx_z + logz - logqz_x)
-
-
-@tf.function
-def train_step(model, x, optimizer):
-    """Executes one training step and returns the loss.
-
-    This function computes the loss and gradients, and uses the latter to update the model's parameters
-    """
-    with tf.GradientTape() as tape:
-        loss = compute_loss(model, x)
-    gradients = tape.gradient(loss, model.trainable_variables)
-    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-    return loss
-
-
-epochs = 10
-# set the dimensionality of the latent space to a plane for visualization later
-latent_dim = 2
-num_examples_to_generate = 16
-
-# keeping the random vector constant for generation (prediction) so it will be easier to see the improvement
-random_vector_for_generation = tf.random.normal(shape=[num_examples_to_generate, latent_dim])
-
-params = dict()
-params['hidden_dim'] = 300
-params['latent_dim'] = 2
-model = VAE(params)
-
-
-def generate_and_save_images(model, epoch, test_sample):
-    mean, logvar = model.encode(test_sample)
-    z = model.reparametrize(mean, logvar)
-    predictions = model.sample(z)
-    fig = plt.figure(figsize=(4, 4))
-    fig.suptitle(f"epoch:{epoch}")
-
-    for i in range(predictions.shape[0]):
-        plt.subplot(4, 4, i + 1)
-        plt.imshow(predictions[i, :, :, 0], cmap="gray")
-        plt.axis("off")
-
-    # tight_layout minimizes the overlap between 2 sub-plots
-    plt.savefig(f"image_at_epoch_{epoch:04d}")
-    plt.show()
-
-
-# Pick a sample of the best set for generating output images
-assert batch_size >= num_examples_to_generate
-for test_batch in test_dataset.take(1):
-    test_sample = test_batch[0:num_examples_to_generate, :, :, :]
-
-fig = plt.figure(figsize=(4, 4))
-fig.suptitle(f"Real")
-for i in range(num_examples_to_generate):
-    plt.subplot(4, 4, i + 1)
-    plt.imshow(test_sample[i, :, :, 0], cmap="gray")
-    plt.axis("off")
-plt.savefig(f"real")
-plt.show()
-
-generate_and_save_images(model, 0, test_sample)
-
-for epoch in range(1, epochs + 1):
-    start_time = time.time()
-    train_loss = tf.keras.metrics.Mean()
-    for train_x in train_dataset:
-        train_loss(train_step(model, train_x, optimizer))
-    end_time = time.time()
-
-    loss = tf.keras.metrics.Mean()
-    for test_x in test_dataset:
-        loss(compute_loss(model, test_x))
-    train_elbo = -train_loss.result()
-    test_elbo = -loss.result()
-    # display.clear_output(wait=False)
-    print(
-        f"Epoch: {epoch}, Test set ELBO: {test_elbo}, Train set ELBO: {train_elbo}, time elapsed for current epoch: {end_time - start_time}")
-    generate_and_save_images(model, epoch, test_sample)
-
-
-def display_image(epoch_no):
-    return PIL.Image.open(f"image_at_epoch_{epoch_no:04d}.png")
-
-
-plt.imshow(display_image(epoch))
-plt.axis("off")
-plt.show()
-
-anim_file = "cvae.gif"
-
-with imageio.get_writer(anim_file, mode="I") as writer:
-    filenames = glob.glob("image*.png")
-    filenames = sorted(filenames)
-    for filename in filenames:
-        image = imageio.imread(filename)
-        writer.append_data(image)
-    image = imageio.imread(filename)
-    writer.append_data(image)
-
-
-# import tensorflow_docs.vis.embed as embed
-# embed.embed_file(anim_file)
-
-
-def plot_latent_images(model, n, digit_size=28):
-    """Plots n x n digit images decoded from the latent space."""
-
-    norm = tfp.distributions.Normal(0, 1)
-    grid_x = norm.quantile(np.linspace(0.05, 0.95, n))
-    grid_y = norm.quantile(np.linspace(0.05, 0.95, n))
-    image_width = digit_size * n
-    image_height = image_width
-    image = np.zeros((image_height, image_width))
-
-    for i, yi in enumerate(grid_x):
-        for j, xi in enumerate(grid_y):
-            z = np.array([[xi, yi]])
-            x_decoded = model.sample(z)
-            digit = tf.reshape(x_decoded[0], (digit_size, digit_size))
-            image[i * digit_size: (i + 1) * digit_size,
-            j * digit_size: (j + 1) * digit_size] = digit.numpy()
-
-    plt.figure(figsize=(10, 10))
-    plt.imshow(image, cmap='Greys_r')
-    plt.axis('Off')
-    plt.show()
-
-
-plot_latent_images(model, 20)
